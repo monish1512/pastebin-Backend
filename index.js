da6d5888c19ef960879640b761ac78d223a12a02
Monish MM <monishmm15@gmail.com.com>
2026-01-08 13:47:57 +0530

    Fix CORS configuration for frontend

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://pastebin-frontend-bice.vercel.app" // future frontend
+    "https://pastebin-frontend-ebon.vercel.app/" // future frontend
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type"],
@@ -27,7 +27,7 @@ app.use("/api", rt);
 app.get("/", (req, res) => {
   res.json({ message: "Pastebin backend running" });
 });
-
+app.options("*", cors());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
